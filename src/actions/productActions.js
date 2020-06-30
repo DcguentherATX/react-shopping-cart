bac6cbd92184fb1bd7eebafec95d8a60ebf89837
David Guenther <davidcarlguenther@gmail.com>
2020-06-30 17:29:01 -0500

    manage orders

--- src/actions/productActions.js
+++ src/actions/productActions.js
@@ -4,7 +4,6 @@ export const fetchProducts = () => async (dispatch) => {
 
     const res = await fetch('/api/products');
     const data = await res.json();
-    console.log(data);
     dispatch({
         type: FETCH_PRODUCTS,
         payload: data
